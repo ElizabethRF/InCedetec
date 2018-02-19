bc6ba41b94351be7236932fec0835af4d813d7d9
ElizabethRF <eli_04nov@hotmail.com>
2018-02-18 23:16:52 -0600

    Initial Commit

---
+++ InCedetec/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InCedetec
+//
+//  Created by Elizabeth Rodríguez Fallas on 18/02/18.
+//  Copyright © 2018 Elizabeth Rodríguez Fallas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

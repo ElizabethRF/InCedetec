99d4ae2935492b4df2e63793dae3ff5acb823c7a
Brandonitas <angiebrand10@hotmail.com>
2018-03-30 00:33:59 -0600

    Portal al 50%, solo el cubo

--- InCedetec/PortalViewController.swift
+++ InCedetec/PortalViewController.swift
@@ -23,8 +23,33 @@ class PortalViewController: UIViewController, ARSCNViewDelegate {
         self.configuration.planeDetection = .horizontal
         self.sceneView.session.run(configuration)
         self.sceneView.delegate = self
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
 
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func handleTap(sender : UITapGestureRecognizer){
+        guard let sceneView = sender.view as? ARSCNView else{return}
+        let touchLocation = sender.location(in: sceneView)
+        let hitTestResult = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+        if !hitTestResult.isEmpty{
+            self.addPortal(hitTestResult: hitTestResult.first!)
+        }else{
+            
+        }
+    }
+    
+    func addPortal(hitTestResult:ARHitTestResult){
+        let portalScene = SCNScene(named: "Portal.scncassets/Portal.scn")
+        let portalNode = portalScene?.rootNode.childNode(withName: "Portal", recursively: false)
+        let transform = hitTestResult.worldTransform
+        let planeXposition = transform.columns.3.x
+        let planeYposition = transform.columns.3.y
+        let planeZposition = transform.columns.3.z
+        portalNode?.position = SCNVector3(planeXposition,planeYposition,planeZposition)
+        self.sceneView.scene.rootNode.addChildNode(portalNode!)
+        
     }
 
     override func didReceiveMemoryWarning() {

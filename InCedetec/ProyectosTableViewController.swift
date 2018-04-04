3343c3bb54d2d9a53cadc80af58c7ffac576660d
Abraham Silva <abraham@tenatek.com>
2018-04-04 04:13:19 -0500

    base de datos

--- InCedetec/ProyectosTableViewController.swift
+++ InCedetec/ProyectosTableViewController.swift
@@ -7,10 +7,11 @@
 //
 
 import UIKit
+import CoreData
 
 class ProyectosTableViewController: UITableViewController{
     
-    var projects : [String] = [];
+    var projects : [NSManagedObject] = [];
     let cellId = "cellId";
     
     override func viewDidLoad() {
@@ -19,12 +20,25 @@ class ProyectosTableViewController: UITableViewController{
         tableView.register(UITableViewCell.self, forCellReuseIdentifier:cellId);
       
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Project")
+        
+        do {
+            projects = try managedContext.fetch(fetchRequest)
+        }catch let err as NSError {
+            print("fallo el fetch", err)
+        }
+    }
+    
     @objc func addProject(_ sender: Any) {
         let alertController = UIAlertController(title: "Agregar nuevo proyecto", message: "Nombre del proyecto", preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "Agregar", style: .default) { [unowned self] action in
             guard let textField = alertController.textFields?.first, let projectToAdd = textField.text else{ return}
-            self.projects.append(projectToAdd)
+            self.save(projectToAdd)
             self.tableView.reloadData()
         }
         let cancelAction = UIAlertAction(title: "Cancelar", style: .destructive, handler: nil)
@@ -35,6 +49,22 @@ class ProyectosTableViewController: UITableViewController{
         
     }
     
+    func save(_ projectName: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Project", in: managedContext)!
+        let project = NSManagedObject(entity: entity, insertInto: managedContext)
+        project.setValue(projectName, forKey: "projectName")
+        
+        do {
+            try managedContext.save()
+            projects.append(project)
+        }catch let err as NSError {
+            print("Fallo al guardar proyecto", err)
+        }
+        
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return projects.count;
     }
@@ -42,7 +72,7 @@ class ProyectosTableViewController: UITableViewController{
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
         let project = projects[indexPath.row]
-        cell.textLabel?.text = project
+        cell.textLabel?.text = project.value(forKey: "projectName") as? String
         return cell
     }
 }
